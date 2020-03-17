70a1b2506794d69c014f22f0b04517ea1313f53f
Jeremiah Norris <jjno91@gmail.com>
2020-03-17 18:38:26 -0500

    output rds_port

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "rds_security_group_id" {
   description = "https://www.terraform.io/docs/providers/aws/r/security_group.html#id"
   value       = aws_security_group.rds.id
 }
+
+output "rds_port" {
+  description = "https://www.terraform.io/docs/providers/aws/r/rds_cluster.html#port-1"
+  value       = aws_rds_cluster.this.port
+}
