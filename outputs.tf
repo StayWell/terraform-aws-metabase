ac9a0a6659fc3637b37f2a25544ed1b43bce3cdc
Jeremiah Norris <jjno91@gmail.com>
2020-03-24 21:37:22 -0500

    expose target_group_arn

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "listener_arn" {
   description = "https://www.terraform.io/docs/providers/aws/r/lb_listener.html#arn"
   value       = aws_lb_listener.https.arn
 }
+
+output "target_group_arn" {
+  description = "https://www.terraform.io/docs/providers/aws/r/lb_target_group.html#arn"
+  value       = aws_lb_target_group.this.arn
+}
